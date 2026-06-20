a8a3d1596a081a6885ff49da4ca50f9493a13430
soumikapentaboyina <pentaboyinas@gmail.com>
2026-06-20 19:43:01 +0530

    ##6

---
+++ countofasubarray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class countofasubarray{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int arr[] = {1,2,3};
+    int n = arr.length;
+    int count =0;
+            count = n*(n+1)/2;
+System.out.println(count);
+        }
+}
